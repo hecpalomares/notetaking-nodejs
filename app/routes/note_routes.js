ba9667819c85e03ea76eca02d69e7d1065b71775
hecpalomares <hpalomares12@gmail.com>
2017-10-17 23:32:25 -0500

    Delete(Delete) Route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -5,6 +5,7 @@ module.exports = function(app, db) {
 	// Get a note by calling /notes/:id
 	app.get('/notes/:id', (request, response) => {
 		const id = request.params.id;
+		// MongoDB requires a ObjectID not just a string from request.params.id
 		const details = { '_id': new ObjectID(id) };
 		db.collection('notes').findOne(details, (err, item) => {
 			if(err) {
@@ -14,6 +15,19 @@ module.exports = function(app, db) {
 			}
 		});
 	});
+
+	// Delete a note by calling /notes/:id
+	app.delete('/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        response.send({'error':'An error has occurred'});
+      } else {
+        response.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
 
 	// Push a note by calling /notes
 	app.post('/notes', (request, response) => {
