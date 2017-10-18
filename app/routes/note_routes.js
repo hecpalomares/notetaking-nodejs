7ccec8082910e6208bc3b264bca1c2e444a3a67c
hecpalomares <hpalomares12@gmail.com>
2017-10-17 19:59:01 -0500

    READ(Get) Route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,30 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
-	app.post('/notes', (request, response) => {
-		console.log(request.body)
-		response.send('Hello')
+	
+	// Get a note by calling /notes/:id
+	app.get('/notes/:id', (request, response) => {
+		const id = request.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').findOne(details, (err, item) => {
+			if(err) {
+				response.send({'error': 'An error has occurred'});
+			} else {
+				response.send(item);
+			}
+		});
 	});
+
+	// Push a note by calling /notes
+	app.post('/notes', (request, response) => {
+		const note = { text: request.body.body, title: request.body.title };
+		db.collection('notes').insert(note, (err, result) => {
+			if(err) {
+				response.send({'error': 'An error has occurred'});
+			} else {
+				response.send(result.ops[0]);
+			}
+		});
+	});
+
 };
